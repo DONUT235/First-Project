01534360344d8aa73715c3b4d71971fab0ed264b
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-04 13:57:40 -0400

    Finish bonus assignments

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', () => {
-    document.querySelector("#Heading2").textContent = "Different Text!"
+let qs = document.querySelector.bind(document);
+qs('#h2btn').addEventListener('click', () => {
+    qs('#heading2').textContent = 'Different Text!';
+});
+qs('#h1frm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    qs('#heading1').textContent = qs('input[name=textInput]').value;
 });
