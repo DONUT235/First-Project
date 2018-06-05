c908429bcd2f2b553ad49cfb70c9fe81e653fed3
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-05 15:07:26 -0400

    Sort spells by level and school

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-let qs = document.querySelector.bind(document);
+const qs = s => document.querySelector(s);
 /*qs('#h2btn').addEventListener('click', () => {
     qs('#heading2').textContent = 'Different Text!';
 });*/
+//Dynamically add multiple spell categories
+(function() {
+    const book = qs('main');
+    for(let i = 1; i <= 9; ++i) {
+      book.innerHTML += `<span class="spellCategory">Level ${i} Spells</span>`
+      book.innerHTML += `<ul id=Level_${i}_spells></ul>`  
+    }
+})();
+
+
+function buildListItem(level) {
+    const newItem = document.createElement('li');
+    const list = qs(`#${level.replace(' ','_')}_spells`);
+    list.appendChild(newItem);
+    console.log(list);
+    return newItem;
+}
+
+function buildSpan(name, school, listItem) {
+    const newSpan = document.createElement('span');
+    newSpan.className = school;
+    newSpan.textContent = name;
+    listItem.appendChild(newSpan);
+}
+
 qs('form').addEventListener('submit', (e) => {
     e.preventDefault();
-    const textInput = e.target.spellName;
-    qs('#spells').innerHTML += `<li>${textInput.value}</li>`;
-    textInput.value = '';
+    const gi = s => e.target[s].value;
+    buildSpan(gi('spellName'),gi('spellSchool'),buildListItem(gi('spellLevel')));
 });
