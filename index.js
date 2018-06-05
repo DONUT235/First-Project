6669e4a41b237375bafbc4847043e57da64897b7
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-05 13:52:42 -0400

    Use CSS given in Slack, and add a dropdown for spell levels

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let qs = document.querySelector.bind(document);
 /*qs('#h2btn').addEventListener('click', () => {
     qs('#heading2').textContent = 'Different Text!';
 });*/
-qs('#h1frm').addEventListener('submit', (e) => {
+qs('form').addEventListener('submit', (e) => {
     e.preventDefault();
-    const textInput = e.target.textInput;
+    const textInput = e.target.spellName;
     qs('#spells').innerHTML += `<li>${textInput.value}</li>`;
     textInput.value = '';
 });
