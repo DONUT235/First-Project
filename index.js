585ec308c4efe42f999eb33a7b027e253d86b792
Aaron Glave <glaveaf@rose-hulman.edu>
2018-06-06 13:30:20 -0400

    Create new spells in SpellBook class but deal with event handlers outside the class

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-function App() {
-    const book = document.querySelector('main');
-    //Dynamically add multiple spell categories
-    for(let i = 1; i <= 9; ++i) {
-      book.innerHTML += `<span class="spellCategory">Level ${i} Spells</span>`;
-      book.innerHTML += `<ul id=Level_${i}_spells></ul>`;  
-    }
-    this.spellList = [];
-    document.querySelector('#addForm').addEventListener('submit', (ev) => {
-        ev.preventDefault();
-        const gi = s => ev.target[s].value;
-        const newSpell = new Spell(gi('spellName'),gi('spellLevel'),gi('spellSchool'));
-        newSpell.makeDomElement();
-        this.spellList.push(newSpell);
-        ev.target.spellName.value = '';
-        ev.target.spellName.focus();
-    });
+function SpellBook() {
+	this.spellList = [];
+	this.addSpell = function(name, level, school) {
+		const newSpell = new Spell(name,level,school);
+		newSpell.makeDomElement();
+		app.spellList.push(newSpell);
+	}
 }
-
 
 function Spell(name, level, school) {
-    this.name = name;
-    this.level = level;
-    this.school = school;
-    this.buildListItem = function() {
-        const newItem = document.createElement('li');
-        const list = document.querySelector(`#${this.level.replace(' ','_')}_spells`);
-        console.log(list);
-        list.appendChild(newItem);
-        return newItem;
-    }
-    this.buildSpan = function() {
-        const newSpan = document.createElement('span');
-        newSpan.className = this.school;
-        newSpan.textContent = this.name;
-        return newSpan;
-    }
-    this.makeDomElement = function () {
-        this.buildListItem().appendChild(this.buildSpan());
-    }
+	this.name = name;
+	this.level = level;
+	this.school = school;
+	this.buildListItem = function() {
+		const newItem = document.createElement('li');
+		const list = document.querySelector(`#${this.level.replace(' ','_')}_spells`);
+		console.log(list);
+		list.appendChild(newItem);
+		return newItem;
+	}
+	this.buildSpan = function() {
+		const newSpan = document.createElement('span');
+		newSpan.className = this.school;
+		newSpan.textContent = this.name;
+		return newSpan;
+	}
+	this.makeDomElement = function () {
+		this.buildListItem().appendChild(this.buildSpan());
+	}
 }
 
-const app = new App();
+
+(function () {
+	const book = document.querySelector('main');
+	//Dynamically add multiple spell categories
+	for(let i = 1; i <= 9; ++i) {
+		book.innerHTML += `<span class="spellCategory">Level ${i} Spells</span>`;
+		book.innerHTML += `<ul id=Level_${i}_spells></ul>`;  
+	}
+	const app = new SpellBook();
+	document.querySelector('#addForm').addEventListener('submit', (ev) => {
+		ev.preventDefault();
+		const gi = s => ev.target[s].value;
+		app.addSpell(gi('spellName'),gi('spellLevel'),gi('spellSchool'));
+		ev.target.spellName.value = '';
+		ev.target.spellName.focus();
+	});
+})();
